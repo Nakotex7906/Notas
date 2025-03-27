1c82bd97ca0270e2c3832490e4a8abf15bbc3bbb
Ignacio essus <i.essus01@ufromail.cl>
2025-03-27 15:45:47 -0300

    Creacion package imperativo y clases

---
+++ src/main/java/org/example/imperativo/GeneracionDatosImperativo.java
@@ -0,0 +1,4 @@
+package org.example.imperativo;
+
+public class GeneracionDatosImperativo {
+}
---
+++ src/main/java/org/example/imperativo/MainImperativo.java
@@ -0,0 +1,7 @@
+package org.example.imperativo;
+
+public class MainImperativo {
+    public static void main(String[] args) {
+
+    }
+}
